2a08d3c9958243463db95fdfcc43d3413d977cf0
Alexandre <amrm@cin.ufpe.br>
2019-04-22 19:53:33 -0300

    file

--- src/leitor/Reader.java
+++ src/leitor/Reader.java
@@ -1,12 +1,24 @@
 package leitor;
 import tags.Tag;
 import java.util.*;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
+
 public class Reader {
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("QT.txt"), "utf-8"));
 		
 		for(int i = 1; i <= 10; i++) { //inicio round query
+			long startTime =System.currentTimeMillis();
+			int cTotal = 0;
+			writer.write("Teste " + i + "\n");
+			System.out.println("Teste " + i);
 			for(int k = 1; k <= 100; k++) {
 				Tag a = new Tag();			
 				Set<String> M;
@@ -14,14 +26,13 @@ public class Reader {
 				M = a.getTags(i*100);
 				Q.add("0");
 				Q.add("1");
-				int c = 0, sv = 0;
+				int c = 0;
 				while(!M.isEmpty()) {
 					String query = Q.poll();
 					for(String id: M) {
 						if(query.contentEquals(id.subSequence(0, query.length()))) {
 							if(query.length() == 64) {
 								M.remove(query);
-								sv++;
 							}else{
 								c++;
 								Q.add(query+"0");
@@ -32,11 +43,17 @@ public class Reader {
 						
 					}
 				}
+				cTotal+=c;
 				
-			}
-
+			}//fim de 100 simulações por qtd
+			long endTime =System.currentTimeMillis();
+			long totalTime =endTime - startTime;
+			System.out.println(cTotal/100 + " colisoes ");
+			System.out.println(totalTime/1000 + "segundos");
+			writer.write(totalTime/1000 + "segundos : " + cTotal/100 + "colisoes\n");
+			
 		}
-
+		writer.close();
 	}
 
 }
