81f7d24b6e195067e0ff1873c9fddace12f12e5f
Alexandre <amrm@cin.ufpe.br>
2019-04-23 00:32:31 -0300

    qtCorrigido

--- src/leitor/Reader.java
+++ src/leitor/Reader.java
@@ -16,7 +16,7 @@ public class Reader {
 		
 		for(int i = 1; i <= 10; i++) { //inicio round query
 			long startTime =System.currentTimeMillis();
-			int cTotal = 0;
+			int cTotal = 0, sV = 0;
 			writer.write("Teste " + i + "\n");
 			System.out.println("Teste " + i);
 			for(int k = 1; k <= 100; k++) {
@@ -24,33 +24,44 @@ public class Reader {
 				Set<String> M;
 				Queue<String> Q = new LinkedList<>();
 				M = a.getTags(i*100);
+				int c = 0, sv = 0;
+				Iterator<String> it = M.iterator();
 				Q.add("0");
 				Q.add("1");
-				int c = 0;
 				while(!M.isEmpty()) {
+					boolean t = true;
+					if(Q.isEmpty()) break;
 					String query = Q.poll();
-					for(String id: M) {
-						if(query.contentEquals(id.subSequence(0, query.length()))) {
-							if(query.length() == 64) {
-								M.remove(query);
-							}else{
-								c++;
+					int siz = query.length();
+					while(it.hasNext()) {
+						String id = it.next();
+						if(query.contentEquals(id.subSequence(0, siz))) {
+							c++;
+							if(siz == 64) { M.remove(query);  break;} 
+							if(t == true && c > 1) {
 								Q.add(query+"0");
-								Q.add(query+"1");
+								Q.add(query+"1");							
+								t = false; 
 							}
-							break;
-						}
-						
+						}else{
+							if(!t) sv++;
+						}						
 					}
+					cTotal+=c;
+					sV+=sv;
+					c = 0;
+					sv = 0;
+					it = M.iterator();
 				}
-				cTotal+=c;
+				
 				
 			}//fim de 100 simulações por qtd
 			long endTime =System.currentTimeMillis();
 			long totalTime =endTime - startTime;
 			System.out.println(cTotal/100 + " colisoes ");
-			System.out.println(totalTime/1000 + "segundos");
-			writer.write(totalTime/1000 + "segundos : " + cTotal/100 + "colisoes\n");
+			System.out.println(sV/100 + " slots vazios ");
+			System.out.println(totalTime + " milisegundos");
+			writer.write(totalTime/1000 + " milisegundos,  " + cTotal/100 + " colisoes, " +  sV/100 + " slots vazios " +"\n");
 			
 		}
 		writer.close();
